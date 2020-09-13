531985598ae27837b87f8cac2e00f10f6e21788e
Charlton Callender <chacalle@uw.edu>
2020-09-13 16:23:09 -0700

    document installed Python package versions

--- installed_versions/scripts/get_versions.sh
+++ installed_versions/scripts/get_versions.sh
@@ -10,6 +10,9 @@ docker run ihmeuwdemographics/base:$VERSION_TAG R --version >> installed_version
 # output all R packages installed
 docker run -v ${PWD}/installed_versions:/tmp ihmeuwdemographics/base:$VERSION_TAG Rscript /tmp/scripts/get_R_packages.R $VERSION_TAG
 
+# output all Python packages installed
+docker run ihmeuwdemographics/base:$VERSION_TAG pip list >> installed_versions/$VERSION_TAG/Python_packages.txt
+
 # output all apt packages manually installed
 # perl command to remove colour codes https://superuser.com/questions/380772/removing-ansi-color-codes-from-text-stream
 docker run ihmeuwdemographics/base:$VERSION_TAG bash -c "apt list --manual-installed --verbose | perl -pe 's/\e\[[0-9;]*m//g'" >> installed_versions/$VERSION_TAG/apt_packages.txt
